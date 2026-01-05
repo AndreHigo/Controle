5549f7cc670aa4fccd51ce27c96a0f6cbb8cf160
Andre Higo <andre.higo94@gmail.com>
2026-01-05 15:26:03 -0300

    Para de rastrear a pasta .next conforme o .gitignore

--- .next/dev/static/chunks/app_credit-cards_[id]_invoices_page_tsx_95da0365._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/node_modules__pnpm_c5f3d1b3._.js",
-  "static/chunks/_88313c21._.js"
-],
-    source: "dynamic"
-});
--- .next/dev/static/chunks/app_credit-cards_[id]_purchases_page_tsx_95da0365._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/node_modules__pnpm_1d1b39cf._.js",
-  "static/chunks/_249ef832._.js"
-],
-    source: "dynamic"
-});
